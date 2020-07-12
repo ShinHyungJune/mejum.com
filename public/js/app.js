43670189e447f3f2245a3e17ba6553178c268371
ShinHyungJune <ssa4141@naver.com>
2020-07-13 01:44:17 +0900

    change axios setting

--- public/js/app.js
+++ public/js/app.js
@@ -72436,7 +72436,7 @@ window.youtube = {
 };
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.withCredentials = true;
+window.axios.defaults.withCredentials = true;
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
